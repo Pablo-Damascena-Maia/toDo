2b2fdcb4b4f6b81ae140ab8d8d60b47269c18d92
Pablo <pablomaia533@gmail.com>
2025-08-11 09:56:25 -0300

    att

---
+++ script.js
@@ -0,0 +1,42 @@
+const novaTarefa = document.querySelector("[data-form-button]");
+
+
+
+
+novaTarefa.addEventListener("click",()=>{
+   const tarefa = document.querySelector("[data-form-imput]")
+   const valor = tarefa.value;
+   console.log(valor); 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//evento
+//onde vai acontecer o evento - butao
+//qual evento vai acontecer - click
+//oq vai acontecer - fui clicado - ação => função.
+
+//function somar(num1,num2){
+  // console.log(num1+num2);
+//}
+//somar(20,20);
+//const multiplicar = (num1,num2 ) => console.log(num1*num2);
+
+//multiplicar(10,10);
